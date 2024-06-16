82058a22600bf7c359375a6657682e5d9e51f733
unknown <mitrovicbojan87@gmail.com>
2024-06-16 22:29:11 +0300

    search

--- bmi.js
+++ bmi.js
@@ -3,6 +3,11 @@
   between 18.5 and 24.9 – This is described as the 'healthy range'. 
   between 25 and 29.9 – This is described as overweight. 
   between 30 and 39.9 – This is described as obesity.
+
+  function isInt(value) {
+    var er = /^-?[0-9]+$/;
+    return er.test(value);
+}
    
  */
 
@@ -13,11 +18,20 @@ let btnBmi = document.getElementById("btn-bmi");
 let bmiRes = document.getElementById("bmi-result");
 
 btnBmi.addEventListener("click", function () {
-  let weight = document.getElementById("weight").value;
-  let height = document.getElementById("height").value;
+  let weight = Number(document.getElementById("weight").value);
+  let height = Number(document.getElementById("height").value);
 
-  console.log(weight);
-  let result = (Number(weight) / (Number(height) * Number(height))) * 10000;
+  console.log(typeof weight);
+  console.log(typeof height);
+
+  // if (
+  //   (typeof weight != "number" && typeof height != "number") ||
+  //   (weight <= 0 && height <= 0)
+  // ) {
+  //   document.getElementById("weight").style.borderColor = "red";
+  // }
+
+  let result = (weight / (height * height)) * 10000;
 
   bmiRes.innerHTML = `Your BMI is ${result.toFixed(2)}`;
 });
