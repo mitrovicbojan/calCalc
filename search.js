1f067d79216353967fa276ba07abd1b8f895954f
unknown <mitrovicbojan87@gmail.com>
2024-06-30 15:52:47 +0300

    popup

--- search.js
+++ search.js
@@ -57,19 +57,41 @@ function recipeInstruction(meal) {
             <p class="recipe-category">${meal.strCategory}</p>
             <h2 class="recipe-title">${meal.strMeal}</h2>            
             <div class="instructions">
-              <h3 class="instructions-title">Instructions</h3>
-              <p class="recipe-description">${meal.strInstructions}</p>
-            </div>
-            <div class="recipe-image">
-              <img
-                class="recipe-details-img"
-                src="${meal.strMealThumb}"
-                alt="food"
-              />
-            </div>
-            <div class="recipe-video">
-              <a href="${meal.strYoutube}" class="video-link">Watch Video</a>
-            </div>
+              
+              <div class="ingredient-box">
+                <h3 class="instructions-title">Ingredients:</h3>
+                <ul class="ingredient-list">
+                  <li >${meal.strMeasure1} ${meal.strIngredient1}</li>
+                  <li >${meal.strMeasure2} ${meal.strIngredient2}</li>
+                  <li >${meal.strMeasure3} ${meal.strIngredient3}</li>
+                  <li >${meal.strMeasure4} ${meal.strIngredient4}</li>
+                  <li >${meal.strMeasure5} ${meal.strIngredient5}</li>
+                  <li >${meal.strMeasure6} ${meal.strIngredient6}</li>
+                  <li >${meal.strMeasure7} ${meal.strIngredient7}</li>
+                  <li >${meal.strMeasure8} ${meal.strIngredient8}</li>
+                  <li >${meal.strMeasure9} ${meal.strIngredient9}</li>
+                  <li >${meal.strMeasure10} ${meal.strIngredient10}</li>
+                  <li >${meal.strMeasure11} ${meal.strIngredient11}</li>
+                  <li >${meal.strMeasure12} ${meal.strIngredient12}</li>
+                  <li >${meal.strMeasure13} ${meal.strIngredient13}</li>
+                  <li >${meal.strMeasure14} ${meal.strIngredient14}</li>
+                  <li >${meal.strMeasure15} ${meal.strIngredient15}</li>
+                  <li >${meal.strMeasure16} ${meal.strIngredient16}</li>
+                  <li >${meal.strMeasure17} ${meal.strIngredient17}</li>
+                  <li >${meal.strMeasure18} ${meal.strIngredient18}</li>
+                  <li >${meal.strMeasure19} ${meal.strIngredient19}</li>
+                  <li >${meal.strMeasure20} ${meal.strIngredient20}</li>
+                </ul>
+              </div>
+              <div class="instructions-box">
+                <h3 class="instructions-title">Instructions:</h3>
+                <p class="recipe-description">${meal.strInstructions}</p>
+                <div class="recipe-video">
+                  <a href="${meal.strYoutube}" class="video-link" target="_blank" rel="noopener noreferrer">Watch Video</a>
+                </div>
+              </div>            
+            </div>            
+            
          
   `;
   mealDispaly.innerHTML = html;
